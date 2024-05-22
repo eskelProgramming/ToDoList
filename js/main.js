489bf3aafb30a184ba9c31e554cd927beb591086
eskelProgramming <eskelley0673@students.cptc.edu>
2024-05-21 21:34:56 -0700

    Refactor task submission logic and add local storage support

--- js/main.js
+++ js/main.js
@@ -1,15 +1,33 @@
 window.onload = function () {
     let submitTaskButton = document.querySelector("#task-submit-btn");
-    submitTaskButton.addEventListener("click", validateTask);
+    submitTaskButton.onclick = function () {
+        addTask();
+    };
 };
+function addTask() {
+    let task = validateTask();
+    if (task != null) {
+        addTaskToLocalStorage(task);
+    }
+}
 function validateTask() {
     let taskInput = document.getElementById("task");
-    let task = taskInput.value;
-    task = task.trim();
-    if (task == "") {
+    let taskName = taskInput.value;
+    taskName = taskName.trim();
+    if (taskName == "") {
         alert("Please enter a task");
+        return null;
     }
     else {
-        let newTask = new Task(task);
+        let newTask = new Task(taskName);
+        return newTask;
     }
+}
+function addTaskToLocalStorage(t) {
+    const taskStorageKey = "Tasks";
+    let taskData = localStorage.getItem(taskStorageKey);
+    let tasks = taskData ? JSON.parse(taskData) : [];
+    tasks.push(t);
+    taskData = JSON.stringify(tasks);
+    localStorage.setItem(taskStorageKey, taskData);
 }
--- ts/main.ts
+++ ts/main.ts
@@ -1,18 +1,60 @@
 window.onload = function() {
     let submitTaskButton: HTMLButtonElement = document.querySelector("#task-submit-btn") as HTMLButtonElement;
-    submitTaskButton.addEventListener("click", validateTask);
+    submitTaskButton.onclick = function() {
+        addTask();
+    }
 }
 
-function validateTask() {
-    let taskInput: HTMLInputElement = <HTMLInputElement>document.getElementById("task");
-    let task: string = taskInput.value;
-    task = task.trim();
+/**
+ * If the task is valid, add it to the local storage
+ */
+function addTask() {
+    let task: Task|null = validateTask();
 
-    if (task == "") {
+    if (task != null){
+        addTaskToLocalStorage(task);
+    }
+}
+
+/**
+ * Validate the task input to make sure it isn't empty
+ * create a new Task object if the input is valid
+ * return null if the input is invalid
+ * @returns Task|null Task object if input is valid, null otherwise
+ */
+function validateTask(): Task|null{
+    let taskInput: HTMLInputElement = <HTMLInputElement>document.getElementById("task");
+    let taskName: string = taskInput.value;
+    taskName = taskName.trim();
+
+    if (taskName == "") {
         alert("Please enter a task");
+        return null;
     }
     else {
-        let newTask = new Task(task);
+        let newTask = new Task(taskName);
+        // newTask.name = taskName;
+        return newTask;
     }
 }
 
+/**
+ * Adds a task to the local storage
+ * @param t Task to add to local storage
+ */
+function addTaskToLocalStorage(t: Task) {
+    const taskStorageKey = "Tasks";
+    // Read the existing tasks from storage
+    let taskData = localStorage.getItem(taskStorageKey);
+
+    // Initialize with existing taskData if is not null, or an empty array
+    // This is a JS ternary/conditional operator
+    let tasks: Task[] = taskData ? JSON.parse(taskData) : [];
+
+    // Add the new task to the list
+    tasks.push(t);
+
+    // Add to localStorage
+    taskData = JSON.stringify(tasks);
+    localStorage.setItem(taskStorageKey, taskData);
+}
